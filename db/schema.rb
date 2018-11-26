f01b224e61f288136a6b433639597b28e9c740df
profh <profh@cmu.edu>
2018-11-26 11:45:11 -0500

    Set up migrations for multisearch

--- db/schema.rb
+++ db/schema.rb
@@ -116,6 +116,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_023435) do
     t.index ["owner_id"], name: "index_pets_on_owner_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
   create_table "procedure_costs", force: :cascade do |t|
     t.bigint "procedure_id"
     t.integer "cost"
